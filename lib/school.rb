490467b0c6d5a8bc4121e55885ebf92ab28b3834
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-04-29 23:49:59 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -16,7 +16,17 @@ class School
   end
 
   def grade(grade)
-    roster[grade]
+    @roster[grade]
+  end
+
+  def sort
+    sorted = {}
+
+    @roster.each do |grade, name|
+      sorted[grade] = name.sort
+      end
+      sorted
+    end
   end
 
 end
